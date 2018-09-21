c6f37789726f23d7532e07e9d1895e72aae7688c
Michael J Peppers <mcgdevfunk824@protonmail.com>
2018-09-21 14:29:21 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,23 +7,17 @@ const PATHS = {
   prod: path.resolve(__dirname, 'prod'),
 };
 
-// PRODUCTION CONFIGS
 const productionConfig = merge([
-  // Clean build folder between builds
   parts.clean(PATHS.prod),
-  // Minify Javascript
   parts.minifyJavaScript(),
-  // Output
   {
     output: {
       path: PATHS.prod,
       filename: '[name].prod.js',
     },
     plugins: [
-      // Ignore stuff
       new webpack.IgnorePlugin(/vertx/),
     ],
-    // Exclude NodeModules
     externals: [
       nodeExternals({
         whitelist: [
@@ -36,53 +30,38 @@ const productionConfig = merge([
   },
 ]);
 
-// DEVELOPMENT CONFIGS
 const developmentConfig = merge([
-  // Analyze Bundle
   parts.analyzeBundle(),
-  // Dev Server
   parts.devServer({
     host: 'localhost',
     port: 3000,
   }),
-  // Output
   {
     output: {
       publicPath: 'http://localhost:3000/',
       filename: '[name].dev.js',
     },
-    // Other Plugins
     plugins: [
-      // prints more readable module names in the browser console on HMR updates
       new webpack.NamedModulesPlugin(),
-      // Ignore stuff
       new webpack.IgnorePlugin(/vertx/),
     ],
-    // Ignore all modules in node_modules folder
     externals: [
       nodeExternals({
-        // Except Webpack Hot Devserver & Emitter so
-        // react-hot-loader can work properly
         whitelist: ['webpack/hot/dev-server', 'webpack/hot/emitter'],
       }),
     ],
   },
 ]);
 
-// SHARED CONFIGS
 const commonConfig = merge([
-  // Check Duplicates
   parts.checkDuplicate({
     verbose: true,
     emitError: true,
   }),
-  // Separate source map from bundles
   parts.generateSourceMaps({ type: 'none' }),
   {
     target: 'electron-renderer',
-    // Set Performance Budget
     performance: {
-      // Show performance warning. Can use 'error' as well
       hints: 'warning',
       maxEntrypointSize: 100000, // in bytes
       maxAssetSize: 450000, // in bytes
@@ -124,7 +103,6 @@ const commonConfig = merge([
   },
 ]);
 
-// EXPORT
 module.exports = env => {
   if (env && env.production) {
     return merge(productionConfig, commonConfig);
