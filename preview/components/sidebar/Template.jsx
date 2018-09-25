58a9f447fe3478c9c1f5a7ac67e960f67924b5f9
Michael J Peppers <mcgdevfunk824@protonmail.com>
2018-09-24 23:58:03 -0700

    Create Template.jsx

---
+++ preview/components/sidebar/Template.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Section, Label } from '../shared';
+
+function Template({ t, template, handleInputChange, UILang }) {
+  return (
+    <Section>
+      <Label>
+        {t('preview:sidebar:template', { lng: UILang })}
+      </Label>
+      <select name="template" value={template} onChange={handleInputChange}>
+        <option value="minimal">Minimal</option>
+        <option value="business">Business</option>
+      </select>
+    </Section>
+  );
+}
+
+Template.propTypes = {
+  handleInputChange: PropTypes.func.isRequired,
+  t: PropTypes.func.isRequired,
+  template: PropTypes.string.isRequired,
+  UILang: PropTypes.string.isRequired,
+};
+
+export default Template;
