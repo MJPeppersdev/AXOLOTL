4dead1e38f52393d2efb29fbcd439c26333d0997
Michael J Peppers <mcgdevfunk824@protonmail.com>
2018-09-24 23:54:10 -0700

    Create index.jsx

---
+++ modal/index.jsx
@@ -0,0 +1,10 @@
+require('../libs/dragNdrop.js');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Dialog from './Dialog';
+
+ReactDOM.render(
+    <Dialog />,
+    document.getElementById('root')
+);
