c9de45480883a61c6ac1e0025197614e8f34da48
Michael J Peppers <mcgdevfunk824@protonmail.com>
2018-09-25 18:58:35 -0700

    Create index.jsx

---
+++ preview/templates/minimal/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styled from 'styled-components';
+const Invoice = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
+  padding: 80px 120px;
+  ${props =>
+    props.baseFontSize &&
+    `
+    font-size: ${props.baseFontSize};
+  `} .label {
+    font-family: Montserrat, sans-serif;
+    font-weight: 500;
+    font-size: 0.8em;
+    line-height: 1.75;
+    color: #2c323a;
+    letter-spacing: 1px;
+    margin-bottom: 0.7em;
+    text-transform: uppercase;
+  }
+  p {
+    font-family: Montserrat, sans-serif;
+    font-weight: 300;
+    font-size: 0.8em;
+    color: #71717a;
+    margin: 0;
+    line-height: 1.75;
+  }
+`;
+
+import { setBaseFontSize } from '../../helper';
+
+import Header from './components/Header.jsx';
+import Main from './components/Main.jsx';
+import Footer from './components/Footer.jsx';
+
+function Minimal(props) {
+  return (
+    <Invoice baseFontSize={setBaseFontSize(props.configs.fontSize)}>
+      <Header {...props} />
+      <Main {...props} />
+      <Footer {...props} />
+    </Invoice>
+  );
+}
+
+Minimal.propTypes = {
+  invoice: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+  configs: PropTypes.object.isRequired,
+};
+
+export default Minimal;
