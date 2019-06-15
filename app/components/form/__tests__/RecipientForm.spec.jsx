3aff745d5637190d8d64f994b79e7d6a6ef0f71c
Michael J Peppers <mjpeppersdev@protonmail.com>
2019-06-14 22:00:16 -0700

    Create RecipientForm.spec.jsx

---
+++ app/components/form/__tests__/RecipientForm.spec.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+
+// Component
+import { RecipientForm } from '../RecipientForm.jsx';
+
+// Mocks
+const formData = {
+  fullname: 'Tony Stark',
+  email: 'ironman@starkindustries.com',
+};
+const t = jest.fn();
+const updateRecipientForm = jest.fn();
+
+describe('Recipient Form component', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(
+      <RecipientForm
+        t={t}
+        formData={formData}
+        updateRecipientForm={updateRecipientForm}
+      />
+    );
+  });
+
+  // PROPS & STATE
+  it('receives correct props', () => {
+    expect(wrapper.prop('formData')).toEqual(formData);
+    expect(wrapper.prop('updateRecipientForm')).toEqual(updateRecipientForm);
+  });
+
+  // RENDER
+  it('renders necessary element', () => {
+    expect(wrapper.find('input')).toHaveLength(4);
+  });
+
+  // METHOD
+  // it('handleInputChange correctly', () => {
+    // const fullname = wrapper.find('input').at(0);
+    // fullname.simulate('change', {
+    //   target: { name: 'fullname', value: 'new-name' },
+    // });
+    // Assert
+    // expect(updateRecipientForm).toHaveBeenCalled();
+    // let lastCallArgument =
+    //   updateRecipientForm.mock.calls[
+    //     updateRecipientForm.mock.calls.length - 1
+    //   ][0];
+    // expect(lastCallArgument).toEqual({
+    //   target: {name: 'fullname', value: 'new-name'},
+    // });
+  // });
+
+  // SNAPSHOT
+  it('matches snapshot', () => {
+    const tree = renderer
+      .create(
+        <RecipientForm
+          t={t}
+          formData={formData}
+          updateRecipientForm={updateRecipientForm}
+        />
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
