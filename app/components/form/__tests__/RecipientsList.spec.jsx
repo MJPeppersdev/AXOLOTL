237508367498e32055b3525b34eb8f0e086721bf
Michael J Peppers <mjpeppersdev@protonmail.com>
2019-06-14 22:01:01 -0700

    Create RecipientsList.spec.jsx

---
+++ app/components/form/__tests__/RecipientsList.spec.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+
+// Component
+import { RecipientsList } from '../RecipientsList.jsx';
+
+// Mocks
+const contacts = [
+  {
+    _id: 'first-contact',
+    fullname: 'Tony Stark',
+    company: 'Stark Industries',
+    email: 'ironman@starkindustries.com',
+  },
+  {
+    _id: 'second-contact',
+    fullname: 'Steve Rogers',
+    company: 'SHIELD',
+    email: 'captainamerica@shield.com',
+  },
+];
+const selectedContact = {};
+const updateRecipientList = jest.fn();
+
+describe('Recipient List component', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(
+      <RecipientsList
+        contacts={contacts}
+        selectedContact={selectedContact}
+        updateRecipientList={updateRecipientList}
+      />
+    );
+  });
+
+  // PROPS & STATE
+  it('receives correct props', () => {
+    expect(wrapper.prop('contacts')).toEqual(contacts);
+    expect(wrapper.prop('selectedContact')).toEqual(selectedContact);
+    expect(wrapper.prop('updateRecipientList')).toEqual(updateRecipientList);
+  });
+
+  // RENDER
+  it('renders necessary element', () => {
+    expect(wrapper.find('select')).toHaveLength(1);
+  });
+
+  // LIFE CYCLE EVENTS
+  // TODO
+  it('render when necessary', () => {});
+
+  // PRIVATE METHOD
+  it('handleInputChange correctly', () => {
+    const input = wrapper.find('select');
+    input.simulate('change', { target: { value: 'second-contact' } });
+    expect(updateRecipientList).toHaveBeenCalled();
+  });
+
+  // SNAPSHOT
+  it('matches snapshot', () => {
+    const tree = renderer
+      .create(
+        <RecipientsList
+          contacts={contacts}
+          selectedContact={selectedContact}
+          updateRecipientList={updateRecipientList}
+        />
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
