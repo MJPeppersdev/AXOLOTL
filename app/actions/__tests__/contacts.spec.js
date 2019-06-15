55ada9d987101d67ee71f3064b7093cf315d2af9
Michael J Peppers <mjpeppersdev@protonmail.com>
2019-06-14 18:25:30 -0700

    Create contacts.spec.js

---
+++ app/actions/__tests__/contacts.spec.js
@@ -0,0 +1,27 @@
+import * as ACTION_TYPES from '../../constants/actions.jsx';
+import * as actions from '../contacts';
+
+it('getAllContacts should create GET_ALL_CONTACTS action', () => {
+    expect(actions.getAllContacts()).toEqual({
+        type: ACTION_TYPES.CONTACT_GET_ALL,
+    });
+});
+
+it('saveContact should create SAVE_CONTACT action', () => {
+    const contactData = {
+        _id: 'tony_stark',
+        fullname: 'Tony Stark',
+        email: 'ironman@avengermail.com',
+    };
+    expect(actions.saveContact(contactData)).toEqual({
+        type: ACTION_TYPES.CONTACT_SAVE,
+        payload: contactData,
+    });
+});
+
+it('deleteContact should create DELETE_CONTACT action', () => {
+    expect(actions.deleteContact('tony_stark')).toEqual({
+        type: ACTION_TYPES.CONTACT_DELETE,
+        payload: 'tony_stark',
+    });
+});
