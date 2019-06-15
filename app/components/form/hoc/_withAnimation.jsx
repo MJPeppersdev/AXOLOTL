5143eee1242a21680f34757f6664d1e8b088b4c3
Michael J Peppers <mjpeppersdev@protonmail.com>
2019-06-14 21:50:17 -0700

    Create _withAnimation.jsx

---
+++ app/components/form/hoc/_withAnimation.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+// Animation
+import { Motion, spring } from 'react-motion';
+
+// HOC Component
+const _withAnimation = ComposedComponent => props => {
+  const { index } = props;
+  return (
+    <Motion
+      defaultStyle={{
+        top: index * 50,
+      }}
+      style={{
+        top: spring(index * 50),
+      }}
+    >
+      {({ top, opacity }) => (
+        <div
+          style={{
+            position: 'absolute',
+            width: '100%',
+            top,
+          }}
+        >
+          <ComposedComponent {...props} />
+        </div>
+      )}
+    </Motion>
+  );
+};
+
+export default _withAnimation;
